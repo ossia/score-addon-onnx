911b7d39ba903524869ae2f99c96ab5213d1326b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-12-20 16:26:46 -0500

    ci: msvc fix

--- Onnx/helpers/OnnxBase.hpp
+++ Onnx/helpers/OnnxBase.hpp
@@ -4,10 +4,11 @@
 #include <version>
 
 #if !defined(_MSC_VER)
-
-#ifndef _Frees_ptr_opt_
-#define _Frees_ptr_opt_
-#endif
+  #ifndef _Frees_ptr_opt_
+    #define _Frees_ptr_opt_
+  #endif
+#else
+  #define __restrict__
 #endif
 
 #include <onnxruntime_cxx_api.h>
