677789b17275a68daf7496f3e2d92868a25ff746
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-06-11 15:47:27 -0400

    feature: implement wekinator-like regressor and classifier thanks to rapidlib

---
+++ RapidlibModels/Classifier.cpp
@@ -0,0 +1,55 @@
+#include "Classifier.hpp"
+
+#include "src/classification.h"
+
+namespace RapidlibModels
+{
+Classifier::Classifier() noexcept { }
+Classifier::~Classifier() { }
+void Classifier::operator()()
+{
+  if (inputs.undo)
+  {
+    if (!m_trainingSet.empty())
+      m_trainingSet.pop_back();
+  }
+
+  if (inputs.reset)
+  {
+    m_trainingSet.clear();
+  }
+
+  if (inputs.record)
+  {
+    rapidLib::trainingExample ex;
+    ex.input = inputs.input.value;
+    ex.output.reserve(inputs.parameters_i.ports.size());
+    for (auto& port : inputs.parameters_i.ports)
+    {
+      ex.output.push_back(port.value);
+    }
+    m_trainingSet.push_back(std::move(ex));
+  }
+
+  if (inputs.train)
+  {
+    m_trained = m_model.train(m_trainingSet);
+  }
+
+  if (inputs.mode == Mode::Test)
+  {
+    outputs.output.value.clear();
+    for (auto& port : inputs.parameters_i.ports)
+    {
+      outputs.output.value.push_back(port.value);
+    }
+  }
+  else
+  {
+    if (m_trained)
+    {
+      outputs.output.value = m_model.run(inputs.input.value);
+    }
+  }
+}
+}
