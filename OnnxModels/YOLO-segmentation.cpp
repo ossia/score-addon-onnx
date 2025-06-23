87017bd006f3e41b75e74168cc3a55f6494058c7
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-06-23 14:14:29 -0400

    segmentation: add missing files

---
+++ OnnxModels/YOLO-segmentation.cpp
@@ -0,0 +1,83 @@
+#include "YOLO-segmentation.hpp"
+
+#include <boost/algorithm/string.hpp>
+
+#include <QPainter>
+
+#include <Onnx/helpers/Images.hpp>
+#include <Onnx/helpers/OnnxContext.hpp>
+#include <Onnx/helpers/Yolo.hpp>
+namespace OnnxModels
+{
+
+YOLO8Segmentation::YOLO8Segmentation() noexcept
+{
+  inputs.image.request_height = 640;
+  inputs.image.request_width = 640;
+}
+YOLO8Segmentation::~YOLO8Segmentation() = default;
+
+void YOLO8Segmentation::loadClasses()
+{
+  if (!available)
+    return;
+
+  classes.clear();
+  boost::split(
+      classes, this->inputs.classes.file.bytes, boost::is_any_of("\n"));
+}
+
+void YOLO8Segmentation::operator()()
+{
+  auto& in_tex = inputs.image.texture;
+  if (!in_tex.changed)
+    return;
+
+  if (!this->ctx)
+  {
+    this->ctx
+        = std::make_unique<Onnx::OnnxRunContext>(this->inputs.model.file.bytes);
+  }
+  auto& ctx = *this->ctx;
+  auto spec = ctx.readModelSpec();
+  auto t = nchw_tensorFromRGBA(
+      spec.inputs[0],
+      in_tex.bytes,
+      in_tex.width,
+      in_tex.height,
+      this->inputs.resolution.value.x,
+      this->inputs.resolution.value.y,
+      storage,
+      {0.f, 0.f, 0.f},
+      {255.f, 255.f, 255.f});
+  Ort::Value tt[1] = {std::move(t.value)};
+
+  assert(2 == spec.output_names_char.size());
+  Ort::Value out_tt[2]{Ort::Value{nullptr}, Ort::Value{nullptr}};
+  ctx.infer(spec, tt, out_tt);
+
+  std::vector<Yolo::YOLO_segmentation::segmentation_type> ttt;
+  detector.processOutput(
+      classes,
+      out_tt,
+      ttt,
+      640,
+      640,
+      640,
+      640,
+      inputs.iou_threshold,
+      inputs.confidence);
+
+  auto img = Onnx::drawBlobAndSegmentation(
+      inputs.image.texture.bytes, in_tex.width, in_tex.height, ttt);
+
+  outputs.image.create(in_tex.width, in_tex.height);
+  memcpy(
+      outputs.image.texture.bytes,
+      img.bits(),
+      in_tex.width * in_tex.height * 4);
+  outputs.image.texture.changed = true;
+  std::swap(storage, t.storage);
+}
+
+}
---
+++ OnnxModels/YOLO-segmentation.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include <Onnx/helpers/Yolo.hpp>
+#include <OnnxModels/Utils.hpp>
+#include <cmath>
+#include <halp/controls.hpp>
+#include <halp/geometry.hpp>
+#include <halp/meta.hpp>
+#include <halp/sample_accurate_controls.hpp>
+#include <halp/texture.hpp>
+
+namespace Onnx
+{
+struct OnnxRunContext;
+}
+
+namespace OnnxModels
+{
+struct YOLO8Segmentation : OnnxObject
+{
+public:
+  halp_meta(name, "YOLO Segmentation");
+  halp_meta(c_name, "YOLO Segmentation");
+  halp_meta(category, "AI/Computer Vision");
+  halp_meta(author, "YOLO authors, Onnxruntime");
+  halp_meta(description, "Segments objects using a YOLO model.");
+  halp_meta(uuid, "5ab19152-bb7c-4a83-a9aa-ca3021aea528");
+  halp_meta(
+      manual_url,
+      "https://ossia.io/score-docs/processes/"
+      "ai-recognition.html#yolo-segmentation")
+
+      struct
+  {
+    halp::fixed_texture_input<"In"> image;
+    ModelPort model;
+    struct : halp::file_port<"Classes">
+    {
+      void update(YOLO8Segmentation& self) { self.loadClasses(); }
+    } classes;
+    halp::xy_spinboxes_i32<"Model input resolution", halp::range{1, 2048, 640}>
+        resolution;
+    halp::knob_f32<"Min. confidence"> confidence;
+    halp::knob_f32<"IoU threshold"> iou_threshold;
+  } inputs;
+
+  struct
+  {
+    halp::texture_output<"Out"> image;
+
+  } outputs;
+
+  YOLO8Segmentation() noexcept;
+  ~YOLO8Segmentation();
+
+  void operator()();
+
+  void loadClasses();
+
+private:
+  std::unique_ptr<Onnx::OnnxRunContext> ctx;
+  std::vector<std::string> classes;
+  boost::container::vector<float> storage;
+  Yolo::YOLO_segmentation detector;
+};
+}
